15f1558a36d4dda20f5503888f9d3496c0c68e58
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-07-12 11:49:00 -0700

    Created student and advisor classes

---
+++ NestedObjects/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+	/// <summary>
+	/// Represents an individual college student
+	/// </summary>
+	public class Student
+	{
+		/// <summary>
+		/// Legal first name
+		/// </summary>
+		public string? FirstName { get; set; }
+
+		/// <summary>
+		/// Legal last name
+		/// </summary>
+		public string? LastName { get; set; }
+
+		/// <summary>
+		/// Assigned school email address
+		/// </summary>
+		public string? SchoolEmail { get; set; }
+
+		/// <summary>
+		/// Student phone number
+		/// </summary>
+		public string? PhoneNumber { get; set; }
+
+
+		public Advisor AssignedAdvisor { get; set; }
+	}
+}
